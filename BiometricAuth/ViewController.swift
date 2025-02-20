9a05045e4d6ba05e6480bc1e2bc9cbebd63521e7
Swain, Susrut (Cognizant) <Susrut.Swain@otiselevator.net>
2025-02-20 12:41:28 +0530

    Initial Commit

---
+++ BiometricAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BiometricAuth
+//
+//  Created by Swain, Susrut (Cognizant) on 20/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
